bdafa218b912e6ce958a69fdde128861224b4576
username <2365089562@qq.com>
2017-11-15 21:13:23 +0800

    11

---
+++ app/Http/Controllers/FilmUserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class FilmUserController extends Controller
+{
+    //电影院后台首页
+	public function index()
+	{
+		return view('FilmAdmins.index');
+	} 
+
+
+}
